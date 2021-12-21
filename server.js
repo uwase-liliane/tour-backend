87935c3d58395b0af890ead4bac1a9355be40e03
uwase liliane <uliliane906@gmail.com>
2021-12-21 12:16:02 +0200

    this is for updating backend to heroku new

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-Parser";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./src/routes/userRoutes";
